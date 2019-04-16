2fe78c81c9e9673b91465ad29b83dbcda1099da1
LegatLastAbyssWalker <41398173+LegatLastAbyssWalker@users.noreply.github.com>
2019-04-15 20:22:14 -0400

    Update Player.h

--- Player.h
+++ Player.h
@@ -2,7 +2,9 @@
 #define PLAYER_H
 
 #include "SFML/Graphics.hpp"
+
 #include "Coin.h"
+#include "Enemy.h"
 
 class Player {
 	private:
@@ -13,14 +15,15 @@ class Player {
 		Player(sf::Vector2f size);
 		~Player();
 
-
+		
 		//Functions
 		void drawTo(sf::RenderWindow &window);
 		void moveTo(sf::Vector2f distance);
 		void setPlayerPos(sf::Vector2f newPos);
 		int getX();
 		int getY();
-		bool collisionWithCoin(Coin *coin);
+		bool collisionWithCoin(Coin* coin);
+		bool collisionWithEnemy(Enemy* enemy);
 };
 
 #endif
